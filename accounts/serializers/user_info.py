f7dce75323ac1dc8b05b85dbca5c4cc23f1e8d37
Mohammd Hussein <mohammd.hussein04@gmail.com>
2026-03-14 22:40:09 +0200

    user: enhance the user-info api edit response structure

--- accounts/serializers/user_info.py
+++ accounts/serializers/user_info.py
@@ -1,11 +1,20 @@
 from rest_framework import serializers
 
+
 class UserInfoSerializer(serializers.Serializer):
     id = serializers.UUIDField()
     name = serializers.CharField()
     email = serializers.EmailField()
-    roles = serializers.ListField(child=serializers.CharField())
-    primary_role = serializers.CharField()
+    role = serializers.CharField()
 
     coach = serializers.DictField(allow_null=True)
     player = serializers.DictField(allow_null=True)
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        role = data.get("role")
+        if role == "Player":
+            data.pop("coach", None)
+        elif role == "Coach":
+            data.pop("player", None)
+        return data
